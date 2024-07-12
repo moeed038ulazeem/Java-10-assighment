10580ec7aa059ed93a754942f62c251d2d94a79a
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-07-12 03:40:11 -0700

    asiighmint-10

---
+++ java.js
@@ -0,0 +1,36 @@
+///// SMALLEST NUMBER
+//var numbers = []
+//for (var i = 0; i < 5; i++){
+ //   numbers.push(+prompt(`enter your number`));
+//}
+//var smallestNumber = numbers[0];
+//for (var j = 0; j < numbers.length; j++){
+  //  if (numbers[j] < smallestNumber){
+    //    smallestNumber =  numbers[j];
+    //}
+//}
+//console.log(`${smallestNumber}`)
+
+       // reverse array
+
+//var numbers = []
+//for (var i = 0; i < 5; i++){
+ //   numbers.push(+prompt(`enter your number`));
+//}
+
+//for (var j = numbers.length -1; j >= 0; j--){
+  //  console.log(numbers[j]);    
+//}
+
+
+// convert farenhit to cel
+//var tempFahrenheit = +prompt(`enter the temp`);
+//var celsius;
+//celsius = (tempFahrenheit - 32) * 5/9;
+//console.log(celsius);
+
+///convert cel to fahrenhight
+//var temp = +prompt(`enter the temp`);
+//var Fahrenheit;
+//Fahrenheit = (temp * 9/5) + 32;
+//console.log(Fahrenheit);
